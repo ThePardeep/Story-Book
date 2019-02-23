f284acd64c8d16a913c4a6ff86804299a629b467
ThePardeep <pardeepsaini7927@gmail.com>
2019-02-23 14:08:38 +0530

    final commit

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,15 @@
+// module.exports = {
+//     client_id:process.env.GOOGLE_CLIENT_ID,
+//     client_secret:process.env.GOOGLE_CLIENT_SECRET,
+//     callbackURL : '/auth/google/callback',
+//     MongooseURL : process.env.MONGO_URI,
+//     Secret : "CK72??"
+// }
+
 module.exports = {
-    client_id:process.env.GOOGLE_CLIENT_ID,
-    client_secret:process.env.GOOGLE_CLIENT_SECRET,
+    client_id:"63141941185-a9iut1jsr9t2upoqrn6ahepn9mheuq9i.apps.googleusercontent.com",
+    client_secret:"c1hVUIHnhF7GfIBlm7FAC8AD",
     callbackURL : '/auth/google/callback',
-    MongooseURL : process.env.MONGO_URI,
+    MongooseURL : "mongodb://cksb:StoryBook72@ds143474.mlab.com:43474/ckstorybook",
     Secret : "CK72??"
 }
-
