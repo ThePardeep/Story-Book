9a62c3a502a12741f930cf55e4e17c3a82e6522a
Pardeep Saini <40034471+ThePardeep@users.noreply.github.com>
2019-02-23 14:11:18 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,15 +1,7 @@
-// module.exports = {
-//     client_id:process.env.GOOGLE_CLIENT_ID,
-//     client_secret:process.env.GOOGLE_CLIENT_SECRET,
-//     callbackURL : '/auth/google/callback',
-//     MongooseURL : process.env.MONGO_URI,
-//     Secret : "CK72??"
-// }
-
 module.exports = {
-    client_id:"63141941185-a9iut1jsr9t2upoqrn6ahepn9mheuq9i.apps.googleusercontent.com",
-    client_secret:"c1hVUIHnhF7GfIBlm7FAC8AD",
+    client_id:"",
+    client_secret:"",
     callbackURL : '/auth/google/callback',
-    MongooseURL : "mongodb://cksb:StoryBook72@ds143474.mlab.com:43474/ckstorybook",
-    Secret : "CK72??"
+    MongooseURL : "",
+    Secret : ""
 }
